3c0f59a93dfd50057801ec0996fe2ba0c5fecd54
Naziya23 <naziya.xworkz@gmail.com>
2021-09-21 22:15:00 +0530

    added files

---
+++ Accessories.java
@@ -0,0 +1,11 @@
+class Accessories{
+	public static void main(String a[]){
+		String accessoryType="Bangles";
+		String colour="golden";
+		int price=100;
+		System.out.println(accessoryType);
+		System.out.println(colour);
+		System.out.println(price);
+	
+	}
+}
---
+++ Gym.java
@@ -0,0 +1,10 @@
+class Gym{
+	public static void main(String a[]){
+		String gymName = "Quadz Fitness";
+		String area = "Rajajinagar";
+		long contactNo = 88846732L;
+		System.out.println(gymName);
+		System.out.println(area);
+		System.out.println(contactNo);
+	}
+}
