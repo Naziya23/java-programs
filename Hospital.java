91818215ec734728ff8eea51ef7201af8b63cbeb
Naziya23 <naziya.xworkz@gmail.com>
2021-09-22 17:49:14 +0530

    added

---
+++ Hospital.java
@@ -0,0 +1,11 @@
+class Hospitals{
+	public static void main(String a[]){
+		String hospitalName="SDM";
+		String placeName="Hubli";
+		int distance=60;
+		System.out.println(hospitalName);
+		System.out.println(placeName);
+		System.out.println(distance);
+	
+	}
+}
